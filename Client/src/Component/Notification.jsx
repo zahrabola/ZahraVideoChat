6e48b1216d994af41dcc52f34d8a065faef5d7c0
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-07-30 23:09:07 +0100

    Update Notification.jsx

--- Client/src/Component/Notification.jsx
+++ Client/src/Component/Notification.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Button from "@mui/material/Button";
+
+import { SocketContext } from "../SocketContext";
 
 const Notification = () => {
-    return (
-        <div>
-            Notification
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+
+  return (
+    <div>
+      {call.isReceivingCall && !callAccepted && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            padding: "15px",
+          }}
+        >
+          <h3>{call.name} is calling:</h3>
+          <Button variant="contained" color="primary" onClick={answerCall}>
+            Answer
+          </Button>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Notification;
