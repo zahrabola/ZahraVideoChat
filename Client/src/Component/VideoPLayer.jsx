11bc09062b7fa1df51f64185a93bf6d09ae1455d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-07-30 14:56:09 +0100

    Add files via upload

---
+++ Client/src/Component/VideoPLayer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoPLayer = () => {
+    return (
+        <div>
+            Video Playerr 
+        </div>
+    );
+}
+
+export default VideoPLayer;
+///https://www.youtube.com/watch?app=desktop&v=ysTjs_HYq0c 
+//https://www.youtube.com/watch?v=2_ko2IohC1g&t=2s 
