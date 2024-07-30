f6d74ab01669c1a15371a5cf255ff3aa5ff98178
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-07-30 23:07:19 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,27 +2,28 @@ import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import "./App.css";
 import VideoPlayer from "./Component/VideoPlayer";
-import Sidebar from "./Component/Sidebar";
 import Notification from "./Component/Notification";
-import Container from '@mui/material/Container';
-
+import Container from "@mui/material/Container";
+import Sidebar from "./Component/Sidebar";
 
 function App() {
-  //const classes = useStyles();
+
   return (
-    <div >
-      <Container maxWidth="sm">
-      <AppBar className="appBar" position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
-      </AppBar>
-      <VideoPlayer />
-      <Sidebar>
-        <Notification />
-      </Sidebar>
-      </Container>
-     
-    </div>
+    <Container>
+      <div className="wrapper">
+        <AppBar className="appBar" position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Video Chat
+          </Typography>
+        </AppBar>
+        <VideoPlayer />
+        <Sidebar>
+          <Notification />
+        </Sidebar>
+      </div>
+    </Container>
   );
 }
 
 export default App;
+
